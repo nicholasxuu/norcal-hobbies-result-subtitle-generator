b20eba6dd26a24453c19030d9b45969127762ba9
Nicholas Xu <sanfenic@hotmail.com>
2013-04-04 12:06:20 -0700

    updated setting documentation

--- main.php
+++ main.php
@@ -8,15 +8,19 @@ require_once("lib/core.php");
  * setup section start
  */
 
-$filename = ".\\data\\20130404.txt";
-//$subtitle_type = "all"; // all - all racer's data; personal - personal lap data
-$race_name = "4wd Buggy Open A Main";
-$output_filename = "C:\\Users\\Nicholas\\Videos\\20130403-{$race_name}.ass";
-$video_race_start_time = 3;
+//$subtitle_type = "all"; // all - all racer's data; personal - personal lap data // function currently un-developed
+
+
+$filename = ".\\data\\20130404.txt"; // race result's file name
+$race_name = "4wd Buggy Open A Main"; // race wanted to be generated into output
+$output_filename = "C:\\Users\\Nicholas\\Videos\\20130403-{$race_name}.ass"; // output subtitle's file name
+$video_race_start_time = 3; // in seconds, time in video where the tone starts, i.e 3 seconds into the video the tone starts. (note: tone's long, so make sure it's right at the beginning of the tone, maybe 0.1s before the tone starts, otherwise the timing board may looks a bit un-synced.
 $race_length = 5; // in minutes
 
 // set if subtitle_type = personal
-$car_number = 0;
+// $car_number = 0;
+
+
 
 /***
  * setup section ends
